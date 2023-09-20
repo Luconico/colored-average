d18466267143b4b85a4b5361c0047375a1436039
Lucas GS <chambe89@gmail.com>
2023-09-20 11:01:59 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ColoredAverageDirective } from './directives/colored-average.directive';
+import { ExampleComponent } from './components/example/example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColoredAverageDirective,
+    ExampleComponent
   ],
   imports: [
     BrowserModule
