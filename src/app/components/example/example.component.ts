dc189a9599deecb32878921ed76b14c2b7f2913c
Lucas GS <chambe89@gmail.com>
2023-09-20 13:18:32 +0200

    [FEAT] Colored average directive

--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColoredAverageOptions } from '../../directives/colored-average.directive';
 
 @Component({
   selector: 'app-example',
@@ -8,30 +9,42 @@ import { Component } from '@angular/core';
 
 export class ExampleComponent {
 
+  coloredAverageOptions: ColoredAverageOptions = { 
+    minColor: '#FF0000',
+    maxColor: '#00FF00',
+    applyToBackground: true
+  }
+
   rows = [
     {
-      "column1": 0.1,
+      "column1": 0.9,
       "column2": "text",
       "column3": 500,
-      "column4": 20
+      "column4": 1000
     },
     {
       "column1": 0.3,
       "column2": "text",
       "column3": 100,
-      "column4": 320
+      "column4": 3200
     },
     {
       "column1": 0,
       "column2": "text",
       "column3": 200,
-      "column4": -20
+      "column4": -2000
     },
     {
       "column1": 1,
       "column2": "text",
       "column3": 600,
       "column4": 0
+    },
+    {
+      "column1": 2,
+      "column2": "text",
+      "column3": 601,
+      "column4": 500
     }
   ];
 
